d325b1634b3525abe55f169bd0ff0b51740f2e92
Jeong SeongHun <2000tjdgns@naver.com>
2024-09-13 18:31:06 +0900

    feat:프로젝트 생성

---
+++ src/main/java/org/e2cho/e2cho_HW/E2choHwApplication.java
@@ -0,0 +1,13 @@
+package org.e2cho.e2cho_HW;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class E2choHwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(E2choHwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/e2cho/e2cho_HW/E2choHwApplicationTests.java
@@ -0,0 +1,13 @@
+package org.e2cho.e2cho_HW;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class E2choHwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
